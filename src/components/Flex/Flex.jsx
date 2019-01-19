2b21f1b8e9be6661b724ab0842f4917e9c3d74f8
kanehara <kanehara@umich.edu>
2019-01-19 17:15:44 -0600

    better prop spreading for Flex and Ray

--- src/components/Flex/Flex.jsx
+++ src/components/Flex/Flex.jsx
@@ -4,46 +4,15 @@ import Ray from '../Ray'
 
 const Flex = styled(Ray)(({
   inline,
-
-  // Parent properties
-  flow,
-  justifyContent,
-  alignItems,
-  alignContent,
-
-  // Children properties
-  order,
-  flex,
-  alignSelf
+  ...rest
 }) => ({
   display: inline ? 'inline-flex' : 'flex',
-
-  flexFlow: flow,
-  justifyContent,
-  alignItems,
-  alignContent,
-
-  order,
-  flex,
-  alignSelf
+  ...rest
 }))
 
 Flex.propTypes = {
   /** renders as inline-flex if true */
-  inline: PropTypes.bool,
-  display: PropTypes.string,
-  flexDirection: PropTypes.string,
-  flexWrap: PropTypes.string,
-  flexFlow: PropTypes.string,
-  justifyContent: PropTypes.string,
-  alignItems: PropTypes.string,
-  alignContent: PropTypes.string,
-  order: PropTypes.string,
-  flexGrow: PropTypes.string,
-  flexShrink: PropTypes.string,
-  flexBasis: PropTypes.string,
-  flex: PropTypes.string,
-  alignSelf: PropTypes.string
+  inline: PropTypes.bool
 }
 
 /** @component */
--- src/components/Ray/Ray.jsx
+++ src/components/Ray/Ray.jsx
@@ -1,26 +1,6 @@
 import styled from 'styled-components'
-import PropTypes from 'prop-types'
 
-const Ray = styled.div(({
-  position,
-  top,
-  right,
-  bottom,
-  left
-}) => ({
-  position,
-  top,
-  right,
-  bottom,
-  left
-}))
-Ray.propTypes = {
-  position: PropTypes.string,
-  top: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  right: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  bottom: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  left: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
-}
+const Ray = styled.div(props => ({ ...props }))
 
 /** @component */
 export default Ray
